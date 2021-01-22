216afb9d51cd840eb4fbf0a2fdb992444a283d24
jayden-lee <gwanggi.lee@gmail.com>
2021-01-22 23:26:35 +0900

    Add sign-up page

---
+++ src/main/java/com/jayden/inflearn/study/account/SignUpForm.java
@@ -0,0 +1,15 @@
+package com.jayden.inflearn.study.account;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignUpForm {
+
+    private String nickname;
+
+    private String email;
+
+    private String password;
+}
